602a79a589b0127dbb95b289aee7c53bf28485f6
Antti Mattila <antti.mattila@reaktor.fi>
2017-12-21 01:11:20 +0200

    Static dir

--- index.js
+++ index.js
@@ -39,6 +39,6 @@ app.get('/msg', (req, res) => {
   res.redirect('/')
 })
 
-app.use(express.static('.'))
+app.use(express.static('static'))
 
 app.listen(3000, () => console.log('Listening on port 3000!'))
